ec576fa52d21969c51127456dd3b895278ec57fb
mahesh <maheshbankar009@gmail.com>
2026-01-09 15:23:47 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("output from java program");
+        System.out.println(java.time.LocalDateTime.now());
+    }
+}
